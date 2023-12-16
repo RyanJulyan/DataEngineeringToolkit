9c6b8bfe21c8a1d3e0acf1d111254eca8cc56316
Ryan Julyan <ryan@julyan.biz>
2023-12-16 20:51:14 +0200

    update PandasDataFrameTransformations

--- src/data_engineering_toolkit/models/transformations/pandas/pandas_transformations_models.py
+++ src/data_engineering_toolkit/models/transformations/pandas/pandas_transformations_models.py
@@ -1,9 +1,44 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, Optional
+import operator
+from typing import Any, Callable, Dict, Optional
+
+try:
+    from typing import Literal
+except:
+    from typing_extensions import Literal
+
+import pandas as pd
+
+
+def generic_dataframe_column_operation_fact(
+    op: operator, df: pd.DataFrame, column_name: str, fact: Any
+):
+    return op(df[column_name], fact)
+
+
+@dataclass
+class ColumnOperationFact:
+    column_name: str
+    op: operator
+    fact: Any
+
+
+@dataclass
+class ConditionOperation:
+    condition: Callable[
+        [pd.DataFrame], pd.Series
+    ] = generic_dataframe_column_operation_fact
+    operation_type: Literal["filter"] = "filter"
+    kwargs: Optional[Dict[Any, Any]] = field(
+        default_factory=dict
+    )  # Additional Kwargs for function
+
 
 @dataclass
 class HandleMissingDataColumn:
     column_name: str
     method: str
     value: Optional[Any] = None  # Used for fillna
-    kwargs: Optional[Dict[Any, Any]] = field(default_factory=dict)  # Additional Kwargs for function
+    kwargs: Optional[Dict[Any, Any]] = field(
+        default_factory=dict
+    )  # Additional Kwargs for function
--- src/data_engineering_toolkit/services/transformations/pandas/pandas_transformations.py
+++ src/data_engineering_toolkit/services/transformations/pandas/pandas_transformations.py
@@ -6,32 +6,65 @@ from typing import List
 import pandas as pd
 
 # Models
-from data_engineering_toolkit.models.transformations.pandas.pandas_transformations_models import HandleMissingDataColumn
+from data_engineering_toolkit.models.transformations.pandas.pandas_transformations_models import (
+    ConditionOperation,
+    HandleMissingDataColumn,
+)
 
 
 @dataclass
 class PandasDataFrameTransformations:
-  """
+    """
     A class for performing various transformations on a pandas DataFrames in different ways.
     """
 
-  data: pd.DataFrame
+    data: pd.DataFrame
 
-  def select_columns(self,
-                     columns: List[str]) -> PandasDataFrameTransformations:
-    self.data = self.data[columns]
-    return self
+    def select_columns(self, columns: List[str]) -> PandasDataFrameTransformations:
+        """
+        Limit the columns of the DataFrame.
 
-  def select_rows_by_index(self, index):
-    return self.data.loc[index]
+        Args:
+            columns (List[str]): A list of column names to return and modify DataFrame.
 
-  def select_rows_by_condition(self, condition):
-    return self.data[condition]
+        Returns:
+            PandasDataFrameTransformations: The method modifies the DataFrame in place.
+        """
+        self.data = self.data[columns]
+        return self
 
-  def handle_missing_values(
-      self, column_methods: List[HandleMissingDataColumn]
-  ) -> PandasDataFrameTransformations:
-    """
+    def select_rows_by_index(self, index: int):
+        return self.data.loc[index]
+
+    def select_rows_by_conditions(
+        self, condition_operations: List[ConditionOperation]
+    ) -> PandasDataFrameTransformations:
+        """
+        Select rows based on specified conditions and operations.
+
+        This method iterates over a list of ConditionOperation instances,
+        each specifying a condition and an operation to be applied on the DataFrame.
+
+        Args:
+            condition_operations (List[ConditionOperation]): A list of ConditionOperation instances,
+                                                             each specifying a condition and an operation.
+
+        Returns:
+            PandasDataFrameTransformations: The method modifies the DataFrame in place.
+        """
+        for cond_op in condition_operations:
+            if cond_op.condition.__name__ == "generic_dataframe_column_operation_fact":
+                kwargs = {**{"df": self.data}, **cond_op.kwargs}
+                self.data = self.data[cond_op.condition(**kwargs)]
+            elif cond_op.operation_type == "filter":
+                self.data = self.data[cond_op.condition(self.data, **cond_op.kwargs)]
+
+        return self
+
+    def handle_missing_values(
+        self, column_methods: List[HandleMissingDataColumn]
+    ) -> PandasDataFrameTransformations:
+        """
         Handle missing data in specified DataFrame columns using specified methods.
 
         This method iterates over a list of HandleMissingDataColumn instances,
@@ -45,144 +78,201 @@ class PandasDataFrameTransformations:
         Returns:
             PandasDataFrameTransformations: The method modifies the DataFrame in place.
         """
-    for column_method in column_methods:
-      if column_method.column_name in self.data.columns:
-        if column_method.method == "fillna":
-          self.data[column_method.column_name] = self.data[
-              column_method.column_name].fillna(column_method.value,
-                                                **column_method.kwargs)
-        elif column_method.method == "dropna":
-          self.data.dropna(subset=[column_method.column_name],
-                           inplace=True,
-                           **column_method.kwargs)
+        for column_method in column_methods:
+            if column_method.column_name in self.data.columns:
+                if column_method.method == "fillna":
+                    self.data[column_method.column_name] = self.data[
+                        column_method.column_name
+                    ].fillna(column_method.value, **column_method.kwargs)
+                elif column_method.method == "dropna":
+                    self.data.dropna(
+                        subset=[column_method.column_name],
+                        inplace=True,
+                        **column_method.kwargs,
+                    )
 
-    return self
+        return self
 
-  def group_by(self, column):
-    return self.data.groupby(column)
+    def group_by(self, column):
+        return self.data.groupby(column)
 
-  def sort_by_index(self):
-    return self.data.sort_index()
+    def sort_by_index(self):
+        return self.data.sort_index()
 
-  def sort_by_values(self, column):
-    return self.data.sort_values(by=column)
+    def sort_by_values(self, column):
+        return self.data.sort_values(by=column)
 
-  def add_new_column(self, column, values):
-    self.data[column] = values
-    return self.data
+    def add_new_column(self, column, values):
+        self.data[column] = values
+        return self.data
 
-  def drop_column(self, column):
-    return self.data.drop(column, axis=1)
+    def drop_column(self, column):
+        return self.data.drop(column, axis=1)
 
-  def merge_dataframes(self, other_df, on_column, how='inner'):
-    return pd.merge(self.data, other_df, on=on_column, how=how)
+    def merge_dataframes(self, other_df, on_column, how="inner"):
+        return pd.merge(self.data, other_df, on=on_column, how=how)
 
-  def concat_dataframes(self, other_df):
-    return pd.concat([self.data, other_df])
+    def concat_dataframes(self, other_df):
+        return pd.concat([self.data, other_df])
 
-  def create_pivot_table(self, values, index, columns):
-    return self.data.pivot_table(values=values, index=index, columns=columns)
+    def create_pivot_table(self, values, index, columns):
+        return self.data.pivot_table(values=values, index=index, columns=columns)
 
-  def create_crosstab(self, col1, col2):
-    return pd.crosstab(self.data[col1], self.data[col2])
+    def create_crosstab(self, col1, col2):
+        return pd.crosstab(self.data[col1], self.data[col2])
 
-  def apply_function(self, func):
-    return self.data.apply(func)
+    def apply_function(self, func):
+        return self.data.apply(func)
 
-  def string_contains(self, column, substring):
-    return self.data[column].str.contains(substring)
+    def string_contains(self, column, substring):
+        return self.data[column].str.contains(substring)
 
-  def conditional_operation(self, column, condition, true_val, false_val):
-    return np.where(self.data[column] > condition, true_val, false_val)
+    def conditional_operation(self, column, condition, true_val, false_val):
+        return np.where(self.data[column] > condition, true_val, false_val)
 
-  # Aggregation methods
-  def group_by_and_transform(self, group_column, new_column, agg_func):
-    """
-      Applies a transformation function to the specified column, partitioning the data 
-      by the given group column. This is similar to PARTITION BY in SQL.
+    # Aggregation methods
+    def group_by_and_transform(self, group_column, new_column, agg_func):
+        """
+        Applies a transformation function to the specified column, partitioning the data
+        by the given group column. This is similar to PARTITION BY in SQL.
 
-      :param group_column: Column to group by.
-      :param new_column: Name of the new column to be added with the result.
-      :param agg_func: Aggregation function to apply (e.g., 'mean', 'sum', custom function).
-      """
-    self.data[new_column] = self.data.groupby(
-        group_column)[new_column].transform(agg_func)
-    return self.data
+        :param group_column: Column to group by.
+        :param new_column: Name of the new column to be added with the result.
+        :param agg_func: Aggregation function to apply (e.g., 'mean', 'sum', custom function).
+        """
+        self.data[new_column] = self.data.groupby(group_column)[new_column].transform(
+            agg_func
+        )
+        return self.data
 
-  def aggregate_data(self, agg_func):
-    return self.data.aggregate(agg_func)
+    def aggregate_data(self, agg_func):
+        return self.data.aggregate(agg_func)
 
-  def transform(self, column, agg_func):
-    return self.data[column].transform(agg_func)
+    def transform(self, column, agg_func):
+        return self.data[column].transform(agg_func)
 
-  # Row operations
-  def add_row(self, row_data):
-    new_row = pd.DataFrame([row_data], columns=self.data.columns)
-    self.data = pd.concat([self.data, new_row], ignore_index=True)
-    return self.data
+    # Row operations
+    def add_row(self, row_data):
+        new_row = pd.DataFrame([row_data], columns=self.data.columns)
+        self.data = pd.concat([self.data, new_row], ignore_index=True)
+        return self.data
 
-  def modify_row(self, index, row_data):
-    self.data.loc[index] = row_data
-    return self.data
+    def modify_row(self, index, row_data):
+        self.data.loc[index] = row_data
+        return self.data
 
-  def drop_row(self, index):
-    self.data = self.data.drop(index)
-    return self.data
+    def drop_row(self, index):
+        self.data = self.data.drop(index)
+        return self.data
 
-  # Time series analysis
-  def resample_data(self, rule, agg_func='mean'):
-    return self.data.resample(rule).agg(agg_func)
+    # Time series analysis
+    def resample_data(self, rule, agg_func="mean"):
+        return self.data.resample(rule).agg(agg_func)
 
-  def rolling_window(self, window, agg_func='mean'):
-    return self.data.rolling(window=window).agg(agg_func)
+    def rolling_window(self, window, agg_func="mean"):
+        return self.data.rolling(window=window).agg(agg_func)
 
-  # More advanced column operations
-  def arithmetic_operation(self, column, operation, value):
-    if operation == 'add':
-      self.data[column] += value
-    elif operation == 'subtract':
-      self.data[column] -= value
-    elif operation == 'multiply':
-      self.data[column] *= value
-    elif operation == 'divide':
-      self.data[column] /= value
-    return self.data
+    # More advanced column operations
+    def arithmetic_operation(self, column, operation, value):
+        if operation == "add":
+            self.data[column] += value
+        elif operation == "subtract":
+            self.data[column] -= value
+        elif operation == "multiply":
+            self.data[column] *= value
+        elif operation == "divide":
+            self.data[column] /= value
+        return self.data
 
-  # More advanced mapping
-  def map_values(self, column, mapping_dict):
-    self.data[column] = self.data[column].map(mapping_dict)
-    return self.data
+    # More advanced mapping
+    def map_values(self, column, mapping_dict):
+        self.data[column] = self.data[column].map(mapping_dict)
+        return self.data
 
-  # More complex conditional operations
-  def conditional_apply(self, column, condition_func, apply_func):
-    self.data.loc[condition_func(self.data),
-                  column] = self.data.loc[condition_func(self.data),
-                                          column].apply(apply_func)
-    return self.data
+    # More complex conditional operations
+    def conditional_apply(self, column, condition_func, apply_func):
+        self.data.loc[condition_func(self.data), column] = self.data.loc[
+            condition_func(self.data), column
+        ].apply(apply_func)
+        return self.data
 
 
 if __name__ == "__main__":
-  # Example Usage
-  import numpy as np
+    # Example Usage
+    import operator
+    import numpy as np
+    from data_engineering_toolkit.models.transformations.pandas.pandas_transformations_models import (
+        ColumnOperationFact,
+        generic_dataframe_column_operation_fact,
+    )
 
-  data = {
-      'A': [1.23, 2.34, 3.45],
-      'B': [4.567672, 5, 6.78],
-      'C': ['foo', 'bar', 'baz'],
-      'D': ['dog', 'elephant', 'frog'],
-      'E': [1, np.nan, 3],
-      'F': [4, 5, np.nan],
-      "Date": ["2023-01-01", "2023-01-02", "2023-01-03"],
-  }
-  df = pd.DataFrame(data)
-  transformations = PandasDataFrameTransformations(df)
+    data = {
+        "A": [1.23, 2.34, 3.45],
+        "B": [4.567672, 5, 6.78],
+        "C": ["foo", "bar", "baz"],
+        "D": ["dog", "elephant", "frog"],
+        "E": [1, np.nan, 3],
+        "F": [4, 5, np.nan],
+        "Date": ["2023-01-01", "2023-01-02", "2023-01-03"],
+    }
+    df = pd.DataFrame(data)
+    transformations = PandasDataFrameTransformations(df)
 
-  # Apply different missing data handling methods to different columns
-  transformations.handle_missing_data([
-      HandleMissingDataColumn(column_name='E', method="fillna", value=0),
-      HandleMissingDataColumn(column_name='F', method="dropna"),
-  ])
+    # Apply different missing data handling methods to different columns
+    transformations.handle_missing_values(
+        [
+            HandleMissingDataColumn(column_name="E", method="fillna", value=0),
+            HandleMissingDataColumn(column_name="F", method="dropna"),
+        ]
+    )
 
-  print(transformations.data)
-  print()
-  print(transformations.data.dtypes)
+    # Example condition: Select rows where values in column 'E' are greater than 1
+    condition = transformations.data["E"] >= 1
+
+    # Create a ConditionOperation instance using generic_dataframe_column_operation_fact function
+    condition_operation = ConditionOperation(
+        condition=generic_dataframe_column_operation_fact,
+        operation_type="filter",
+        kwargs=ColumnOperationFact(column_name="E", op=operator.ge, fact=1).__dict__,
+    )
+
+    # Apply the select_rows_by_conditions method
+    selected_rows = transformations.select_rows_by_conditions(
+        [condition_operation]
+    ).data
+
+    # Display the selected rows
+    print()
+    print(selected_rows)
+
+    # Define a condition
+    def condition_greater_than_one(df):
+        return df["E"] >= 1
+
+    # Create a ConditionOperation instance
+    condition_operation = ConditionOperation(
+        condition=generic_dataframe_column_operation_fact,
+        operation_type="filter",
+        kwargs=ColumnOperationFact(column_name="E", op=operator.ge, fact=1).__dict__,
+    )
+
+    # Apply the select_rows_by_conditions method
+    selected_rows = transformations.select_rows_by_conditions(
+        [condition_operation]
+    ).data
+
+    # Display the selected rows
+    print()
+    print(selected_rows)
+
+    # Apply different missing data handling methods to different columns
+    transformations.select_columns(
+        [
+            "E",
+            "F",
+        ]
+    )
+    print()
+    print(transformations.data)
+    print()
+    print(transformations.data.dtypes)
