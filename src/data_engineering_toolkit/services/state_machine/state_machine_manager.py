bdfedbd6b8835916f5da34d421ee7250756d2a32
Ryan Julyan <ryan@julyan.biz>
2024-02-23 18:45:42 +0200

    update state machine to follow model logic

--- src/data_engineering_toolkit/services/state_machine/state_machine_manager.py
+++ src/data_engineering_toolkit/services/state_machine/state_machine_manager.py
@@ -3,33 +3,37 @@ from typing import Dict
 
 from statemachine import StateMachine
 
-from data_engineering_toolkit.services.state_machine.managed_state_machine import ManagedStateMachine
-from data_engineering_toolkit.services.state_machine.state_machine_config_model import StateMachineConfig
+from data_engineering_toolkit.models.state_machine.state_machine_config_model import (
+    StateMachineConfig,
+)
+from data_engineering_toolkit.services.state_machine.managed_state_machine import (
+    ManagedStateMachine,
+)
 
 
 @dataclass
 class StateMachineManager:
-  """Manages multiple StateMachine instances, allowing for their creation, modification, and querying."""
-  state_machines: Dict[str, StateMachine] = field(default_factory=dict)
+    """Manages multiple StateMachine instances, allowing for their creation, modification, and querying."""
 
-  def create_state_machine(self, name: str,
-                           config: StateMachineConfig) -> StateMachine:
-    """Creates and registers a new StateMachine based on the provided configuration."""
+    state_machines: Dict[str, StateMachine] = field(default_factory=dict)
 
-    self.state_machines[name] = ManagedStateMachine(name,
-                                                    config).state_machine()
+    def create_state_machine(
+        self, name: str, config: StateMachineConfig
+    ) -> StateMachine:
+        """Creates and registers a new StateMachine based on the provided configuration."""
 
-    return self.state_machines[name]
+        self.state_machines[name] = ManagedStateMachine(name, config).state_machine()
 
-  def get_state_machine(self, name: str) -> StateMachine:
-    """Fetch StateMachine by name."""
+        return self.state_machines[name]
 
-    return self.state_machines[name]
+    def get_state_machine(self, name: str) -> StateMachine:
+        """Fetch StateMachine by name."""
 
-  def trigger_transition(self, name: str, transition_name: str, *args,
-                         **kwargs):
-    return getattr(self.state_machines[name], transition_name)(*args, **kwargs)
+        return self.state_machines[name]
 
-  def get_current_state(self, name: str):
-    """Returns the current state of the specified state machine."""
-    return self.state_machines[name].current_state.id
+    def trigger_transition(self, name: str, transition_name: str, *args, **kwargs):
+        return getattr(self.state_machines[name], transition_name)(*args, **kwargs)
+
+    def get_current_state(self, name: str):
+        """Returns the current state of the specified state machine."""
+        return self.state_machines[name].current_state.id
